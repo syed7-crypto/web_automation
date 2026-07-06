0a3f4acfb0a3124bb7caa311ce64811409a27145
Syed <syed786000000@gmail.com>
2026-07-06 18:46:14 +0530

    Implement entering login details

--- main.py
+++ main.py
@@ -12,3 +12,15 @@ driver = webdriver.Chrome(
 print("Navigating to LinkedIn...")
 driver.get("https://www.linkedin.com/login")
 time.sleep(5)
+
+emailfield = driver.find_element(By.ID, "username")
+emailfield.send_keys("........@gmail.com")
+time.sleep(5)
+
+passwordfield = driver.find_element(By.ID, "password")
+passwordfield.send_keys("password")
+time.sleep(5)
+
+login = driver.find_element(By.ID, "button")
+login.click()
+time.sleep(10)
