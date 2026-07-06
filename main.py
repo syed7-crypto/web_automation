1534d6ef6e37b44baec075909d04b6a97b5367fe
Syed <syed786000000@gmail.com>
2026-07-06 18:40:40 +0530

    Implement LinkedIn navegation

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(
+    service=Service(ChromeDriverManager().install())
+)
+
+print("Navigating to LinkedIn...")
+driver.get("https://www.linkedin.com/login")
+time.sleep(5)
