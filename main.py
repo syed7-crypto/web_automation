8ed767e399639e68e88b5112ad9736ea02c8d4b1
Syed <syed786000000@gmail.com>
2026-07-06 18:35:12 +0530

    Import selenium packages

---
+++ main.py
@@ -0,0 +1,6 @@
+import time
+import random
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
